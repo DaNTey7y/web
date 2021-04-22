5a52782a7877236f891866ca547e2fbd6722b1de
Ainur <xvhuqdph@gmail.com>
2021-04-23 02:09:03 +0500

    -подредачен

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
+from data import db_session
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     param = dict()
     param['title'] = 'Три.ч'
     return render_template('home.html', **param)
 
 
-if __name__ == '__main__':
+def main():
     app.run(port=8080, host='127.0.0.1')
+
+
+if __name__ == '__main__':
+    main()

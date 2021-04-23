b0b38f0805b85c7789d46fbfdb739b5c5a6a1173
Ainur <xvhuqdph@gmail.com>
2021-04-23 13:52:54 +0500

    Класс который хранит информацию о записях будь то ттред или ответ

---
+++ data/posts.py
@@ -0,0 +1,11 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Post(SqlAlchemyBase):
+    __tablename__ = 'posts'
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    type = sqlalchemy.Column(sqlalchemy.String)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    created_time = sqlalchemy.Column(sqlalchemy.Time)

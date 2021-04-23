058a60c2f9769824ab04979577aa466a4487e831
Ainur <xvhuqdph@gmail.com>
2021-04-23 18:35:06 +0500

    подредачен

--- thread_create.py
+++ thread_create.py
@@ -1,4 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField, FileField
 from wtforms.validators import DataRequired
 
+
+class ThreadCreateForm(FlaskForm):
+    title = StringField('Название треда', validators=[DataRequired()])
+    theme = StringField('Тема треда в двух словах', validators=[DataRequired()])
+    text = TextAreaField('Ваш текст...', validators=[DataRequired()])
+    image_1 = FileField('Можете добавить несколько фото')
+    image_2 = FileField()
+    image_3 = FileField()
+    image_4 = FileField()
+    submit = SubmitField('Опубликовать')

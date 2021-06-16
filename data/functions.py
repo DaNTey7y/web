2e146cc15780bbbf79f86f4102de4d6b4fcd0233
Ainur <xvhuqdph@gmail.com>
2021-06-16 23:35:12 +0500

    подправленывырезана функция перемасштабирования картинки за ненадобностью

--- data/functions.py
+++ data/functions.py
@@ -1,18 +1,10 @@
 from random import choice
-from PIL import Image
 
 
 def generate_s(n):
     symbs = 'abcdefghijklmnopqrstuvwxyz0123456789'
     name = ''.join([choice(symbs) for i in range(9)])
     return name
-
-
-def resize_image(path):
-    im = Image.open(path)
-    maxsize = (200, 200)
-    im.thumbnail(maxsize, Image.ANTIALIAS)
-    im.save(path)
 
 
 def form_text(text, n):
